4158391c0022539c8a46846ed0606c860de4ea23
altintasutku <altintasutku@gmail.com>
2024-07-02 00:24:57 +0300

    hash password

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -1,7 +1,10 @@
 import { db } from "@/lib/db";
 import { userTable } from "@/lib/schema";
+import { eq } from "drizzle-orm";
 import { NextResponse } from "next/server";
 import { z } from "zod";
+import bcrypt from "bcryptjs";
+import { env } from "@/env";
 
 export const POST = async (req: Request) => {
   const body = await req.json();
@@ -24,10 +27,32 @@ export const POST = async (req: Request) => {
     );
   }
 
-  const user = await db.insert(userTable).values({
-    email: body.email,
-    password: body.password,
-  }).returning();
+  let user = await db
+    .select()
+    .from(userTable)
+    .where(eq(userTable.email, body.email));
+
+  if (user.length > 0) {
+    return NextResponse.json(
+      {
+        error: "User already exists",
+      },
+      {
+        status: 400,
+      },
+    );
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(body.password, salt);
+
+  user = await db
+    .insert(userTable)
+    .values({
+      email: body.email,
+      password: hashedPassword,
+    })
+    .returning();
 
   if (!user) {
     return NextResponse.json(
